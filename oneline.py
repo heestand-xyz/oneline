695c754617c279d917a62d323ba698396b9198d1
Anton Heestand <anton.heestand@obscuradigital.com>
2016-01-27 12:26:33 -0800

    creates oneline

---
+++ oneline.py
@@ -0,0 +1,21 @@
+import sys
+
+onelinePre = '|:> compressed with oneline https://github.com/Heestand/oneline <:|:> '
+onelinePost = ' <:|'
+
+path = sys.argv[1]
+
+file = open(path, 'r')
+content = file.read()
+file.close()
+
+if int(sys.argv[2]):
+	content = onelinePre + content.replace('\n', '[n]').replace('\t', '[t]').replace('    ', '[t]') + onelinePost
+	print('online compressed')
+else:
+	content = content.replace(onelinePre, '').replace('[n]', '\n').replace('[t]', '\t').replace(onelinePost, '')
+	print('oneline uncompressed')
+
+file = open(path, 'w')
+file.write(content)
+file.close()
